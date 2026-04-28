128763ba9d657b733e8afa6b9050a9aef9810e5e
adwords711-commits <adwords711@gmail.com>
2026-04-27 23:25:51 -0400

    completed js

--- src/js/main.js
+++ src/js/main.js
@@ -3,3 +3,20 @@ import { getParkData } from "./parkService.mjs";
 const parkData = getParkData();
 
 const disclaimer = document.querySelector(".disclaimer > a");
+disclaimer.href = parkData.url;
+disclaimer.innerHTML = parkData.fullName;
+document.title = parkData.YellowstoneNationalPark;
+
+const heroImage = document.querySelector(".hero-banner img");
+heroImage.src = parkData.images[0].url;
+
+function parkInfoTemplate(info) {
+    return `<a href="/" class="hero-banner__title">${info.name}</a>
+    <p class="hero-banner__subtitle">
+        <span>${info.designation}</span>
+        <span>${info.states}</span>
+        </p>`;
+}
+
+document.querySelector(".hero-banner__content").innerHTML =
+    parkInfoTemplate(parkData);
